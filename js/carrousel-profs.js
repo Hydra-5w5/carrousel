84b6a4c612e254f11bbfbfa7580451c49913fb60
Moumy Ndiaye <e2177120@cmaisonneuve.qc.ca>
2023-11-13 18:18:11 -0500

    Carrousel pour la section prof

---
+++ js/carrousel-profs.js
@@ -0,0 +1,39 @@
+(function(){
+  let index = 0; // Initialise l'index à 0
+
+  // Récupère tous les blocflexProfs
+  let blocflexProfs = document.querySelectorAll('.blocflex__profs');
+  
+  let btn__gauche = document.querySelector('.btn__gauche');
+  let btn__droite = document.querySelector('.btn__droite');
+  
+  // Fonction pour mettre à jour les blocflexProfs affichés
+  function mettreAJourAffichage() {
+    // Cache tous les blocflexProfs
+    blocflexProfs.forEach(prof => prof.style.display = 'none');
+  
+    // Affiche trois blocflexProfs
+    for (let i = 0; i < 3; i++) {
+      let prof = blocflexProfs[(index + i) % blocflexProfs.length];
+      if (prof) {
+        prof.style.display = 'flex';
+      }
+    }
+  }
+  
+  // Appelle mettreAJourAffichage initialement pour afficher les trois premiers blocflexProfs
+  mettreAJourAffichage();
+  
+  // Gère le bouton de flèche droite
+  btn__droite.addEventListener('mousedown', function(){
+    index = (index + 1) % blocflexProfs.length;
+    mettreAJourAffichage();
+  })
+  
+  // Gère le bouton de flèche gauche
+  btn__gauche.addEventListener('mousedown', function(){
+    index = (index - 1 + blocflexProfs.length) % blocflexProfs.length;
+    mettreAJourAffichage();
+  })
+}
+)()
