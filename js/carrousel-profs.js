d54dcd924bf2b6a4151970406c56b47f6324bec1
Moumy Ndiaye <112096535+NMoumy@users.noreply.github.com>
2023-11-14 08:23:03 -0500

    test

--- js/carrousel-profs.js
+++ js/carrousel-profs.js
@@ -1,39 +1,40 @@
-(function(){
+(function () {
   let index = 0; // Initialise l'index à 0
 
   // Récupère tous les blocflexProfs
   let blocflexProfs = document.querySelectorAll('.blocflex__profs');
-  
+
   let btn__gauche = document.querySelector('.btn__gauche');
   let btn__droite = document.querySelector('.btn__droite');
-  
+
   // Fonction pour mettre à jour les blocflexProfs affichés
   function mettreAJourAffichage() {
     // Cache tous les blocflexProfs
     blocflexProfs.forEach(prof => prof.style.display = 'none');
-  
+
     // Affiche trois blocflexProfs
-    for (let i = 0; i < 3; i++) {
-      let prof = blocflexProfs[(index + i) % blocflexProfs.length];
+    for (let i = -1; i <= 1; i++) {
+      let profIndex = (index + i + blocflexProfs.length) % blocflexProfs.length;
+      let prof = blocflexProfs[profIndex];
+
       if (prof) {
         prof.style.display = 'flex';
       }
     }
   }
-  
+
   // Appelle mettreAJourAffichage initialement pour afficher les trois premiers blocflexProfs
   mettreAJourAffichage();
-  
+
   // Gère le bouton de flèche droite
-  btn__droite.addEventListener('mousedown', function(){
+  btn__droite.addEventListener('mousedown', function () {
     index = (index + 1) % blocflexProfs.length;
     mettreAJourAffichage();
   })
-  
+
   // Gère le bouton de flèche gauche
-  btn__gauche.addEventListener('mousedown', function(){
+  btn__gauche.addEventListener('mousedown', function () {
     index = (index - 1 + blocflexProfs.length) % blocflexProfs.length;
     mettreAJourAffichage();
   })
-}
-)()
+})();
