4d9d8ccf0aa238aea289e0ce2152eeb1ed955fcf
Moumy Ndiaye <e2177120@cmaisonneuve.qc.ca>
2023-11-25 01:26:29 -0500

    Ajout du code de nazeef

--- js/carrousel-profs.js
+++ js/carrousel-profs.js
@@ -1,23 +1,28 @@
 (function () {
-  console.log("cool");
-  let indice = 1; 
-  let tourFini;
+  let indice = 1; // Initialise l'indice à 1 pour commencer avec le bloc du milieu
+  let tourFini = false; // Vérifie si le carrousel à été vu au complet
+  let recule = false;
+  let avance = true; //
   let indiceProf;
+  let prof;
+  // Récupère tous les professeurs
   let professeurs = document.querySelectorAll('.blocflex__profs');
-
   let boutonGauche = document.querySelector('.btn__gauche');
   let boutonDroite = document.querySelector('.btn__droite');
 
+  // Fonction pour mettre à jour les professeurs affichés
   function mettreAJourAffichage() {
+    // Cache tous les professeurs et supprime les classes spéciales
     professeurs.forEach((prof, i) => {
       prof.style.display = 'none';
       prof.classList.remove('selectionne');
       prof.classList.remove('non-selectionne');
     });
     
+    // Affiche trois professeurs et ajoute les classes spéciales
     for (let i = -1; i <= 1; i++) {
        indiceProf = (indice + i + professeurs.length) % professeurs.length;
-      let prof = professeurs[indiceProf];
+       prof = professeurs[indiceProf];
      
       if (prof) {
         prof.style.display = 'flex';
@@ -27,20 +32,57 @@
           prof.classList.add('non-selectionne'); 
         }
       }
-    }
 
-    if(indice == professeurs.length-1) {
+    }
+/** MAUVAUS CODE QUI FONCTIONNE
+ * Ajoute le prof nécessaire lorsque l'utilisateur recule avec le bouton gauche au lieu d'utiliser les profs générés avec Worpress,
+ * 
+ */
+    if(indiceProf == 0) {
       tourFini = true;
-      insertAfter(professeurs[0],professeurs[indice]);
-     }
-     if(tourFini == true && professeurs[indiceProf+1]) {
-      console.log(professeurs[indiceProf].parentNode);
-      insertAfter(professeurs[indiceProf+1],professeurs[indiceProf]);
-     }
+      insertAfter(prof,professeurs[indice]);
+    }
+    if(tourFini == true) {
+      insertAfter(prof,professeurs[indice]);
+    }
+    if(indiceProf == 1 && tourFini == false )  {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-1],professeurs[indice]);
+    }
+    if(indiceProf == 0 && tourFini == true) {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-2],professeurs[indice]);
+    }
+    if(indiceProf == 8 && tourFini == true) {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-3],professeurs[indice]);
+    }
+    if(indiceProf == 7 && tourFini == true) {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-4],professeurs[indice]);
+    }
+    if(indiceProf == 6 && tourFini == true) {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-5],professeurs[indice]);
+    }
+    if(indiceProf == 5 && tourFini == true) {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-6],professeurs[indice]);
+    }
+    if(indiceProf == 4 && tourFini == true) {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-7],professeurs[indice]);
+    }
+    if(indiceProf == 3 && tourFini == true) {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-8],professeurs[indice]);
+    }
+    if(indiceProf == 2 && tourFini == true) {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-9],professeurs[indice]);
+    }
+    if(indiceProf == 1 && tourFini == true) {
+      prof.parentNode.insertBefore(professeurs[professeurs.length-1],professeurs[indice]);
+    }
+    //console.log("indiceProf: " + indiceProf);
+    //console.log("indice: " + indice);
   }
 
+  // Appelle mettreAJourAffichage initialement pour afficher les trois premiers professeurs
   mettreAJourAffichage();
 
+  // Gère le bouton de flèche droite
   if(boutonDroite) {
     boutonDroite.addEventListener('mousedown', function () {
       indice = (indice + 1) % professeurs.length;
@@ -48,6 +90,8 @@
     })
   }
 
+
+  // Gère le bouton de flèche gauche
   if (boutonGauche) {
     boutonGauche.addEventListener('mousedown', function () {
       indice = (indice - 1 + professeurs.length) % professeurs.length;
